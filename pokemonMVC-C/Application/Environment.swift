236c4362e13b0fc3e65f20236fbb0d834b88fd3d
Gabriel Marson <gabrielmarson95@gmail.com>
2018-03-30 14:12:29 -0300

    initial commit

---
+++ pokemonMVC-C/Application/Environment.swift
@@ -0,0 +1,20 @@
+//
+//  Environment.swift
+//  pokemonMVC-C
+//
+//  Created by Gabriel M on 3/30/18.
+//  Copyright © 2018 Gabriel M. All rights reserved.
+//
+
+import UIKit
+
+class Environment {
+    
+    // MARK: - Singleton
+    static let shared = Environment()
+    
+    // MARK: - Properties
+    var baseURL: String! {
+        return "theBaseUrl"
+    }
+}
