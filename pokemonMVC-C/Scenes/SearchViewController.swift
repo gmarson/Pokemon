f9347f7360bf0210639c22f3052f683c42e9f64a
Gabriel Marson <gabrielmarson95@gmail.com>
2018-07-02 10:47:34 -0300

    build layout for search screen

--- pokemonMVC-C/Scenes/SearchViewController.swift
+++ pokemonMVC-C/Scenes/SearchViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pikachuStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
