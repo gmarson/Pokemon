8b4eabd54e3ce0036b5cfcf588aa3b7d652e563a
gmarson <gabrielmarson@live.com>
2019-03-23 20:10:56 -0300

    removing keychain and added realm

---
+++ pokemonMVC-C/Conveniences/Realm/PokemonRealmPersistency.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonRealmPersistency.swift
+//  pokemon
+//
+//  Created by Gabriel M on 3/23/19.
+//  Copyright © 2019 Gabriel M. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class PokemonRealmPersistency {
+    
+}
