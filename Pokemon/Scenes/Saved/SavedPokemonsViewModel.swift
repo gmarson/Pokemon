aeec7a78596e31c484f920df5cf45b5458fc42af
Gabriel Marson <gabriel@gabriel-marson.local>
2019-03-27 17:40:57 -0300

    created viewmodel and added rxswift

---
+++ Pokemon/Scenes/Saved/SavedPokemonsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SavedPokemonsViewModel.swift
+//  Pokemon
+//
+//  Created by Gabriel Marson on 27/03/19.
+//  Copyright © 2019 Gabriel Marson. All rights reserved.
+//
+
+import Foundation
+
+class SavedPokemonsViewModel {
+    
+}
