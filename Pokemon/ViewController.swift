4e44522d73c6476ac982de2156887ff0b60fe14e
Gabriel Marson <gabriel@gabriel-marson.local>
2019-03-27 12:05:40 -0300

    changed folder names

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Gabriel Marson on 27/03/19.
+//  Copyright © 2019 Gabriel Marson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- PokemonTests/PokemonTests.swift
+++ PokemonTests/PokemonTests.swift
@@ -1,36 +1,34 @@
 //
-//  pokemonMVC_CTests.swift
-//  pokemonMVC-CTests
+//  PokemonTests.swift
+//  PokemonTests
 //
-//  Created by Gabriel M on 3/30/18.
-//  Copyright © 2018 Gabriel M. All rights reserved.
+//  Created by Gabriel Marson on 27/03/19.
+//  Copyright © 2019 Gabriel Marson. All rights reserved.
 //
 
 import XCTest
-@testable import pokemonMVC_C
+@testable import Pokemon
 
-class pokemonMVC_CTests: XCTestCase {
-    
+class PokemonTests: XCTestCase {
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
